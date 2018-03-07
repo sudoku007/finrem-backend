c03cee3fd37aec02ac7bd92fea635185f8705eb5
rafalkalita <rkalita@equalexperts.com>
2018-03-07 09:45:11 +0000

    Fixed module source

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,9 +1,11 @@
 module "finrem-backend" {
-  source = "git@github.com:hmcts/finrem-backend.git?ref=master"
+  source = "git@github.com:contino/moj-module-webapp?ref=master"
   product = "${var.product}-backend"
   location = "${var.location}"
   env = "${var.env}"
   ilbIp = "${var.ilbIp}"
+  subscription = "${var.subscription}"
+  is_frontend  = false
 
   app_settings = {
     MANAGEMENT_SECURITY_ENABLED = "${var.management_security_enabled}"
